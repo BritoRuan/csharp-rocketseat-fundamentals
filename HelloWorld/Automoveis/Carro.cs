e80ac2e12d73ffb4057ad37a789e5838f8145b4b
Ruan Brito <ruan.desenvolvedor@gmail.com>
2025-04-01 16:54:17 -0300

    Add project files.

---
+++ HelloWorld/Automoveis/Carro.cs
@@ -0,0 +1,23 @@
+﻿namespace HelloWorld.Automoveis;
+class Carro
+{
+    public void Ligar()
+    {
+        Console.WriteLine("Carro ligado");
+    }
+
+    public void Desligar()
+    {
+        Console.WriteLine("Carro desligado");
+    }
+
+    internal void NumeroDoMotor()
+    {
+        Console.WriteLine("V8 6.0L");
+    }
+
+    private void NumeroDoChassi()
+    {
+        Console.WriteLine("8LS 4g323v lV 0t5676");
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using HelloWorld.Automoveis;
+using HelloWorld.Comidas;
+
+namespace HelloWorld;
+
+class Program 
+{
+    static void Main()
+    {
+        Carro meuCarro = new Carro();
+
+        meuCarro.Ligar();
+
+        meuCarro.Desligar();
+
+        Biscoito meuBiscoito = new Biscoito();
+
+        meuBiscoito.Temperatura();
+    }
+}
