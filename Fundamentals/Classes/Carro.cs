3bf64d21dc263080a82fbcfad528ee14e32d4f6c
Ruan Brito <ruan.desenvolvedor@gmail.com>
2025-04-02 17:56:34 -0300

    refactor: updated car class

---
+++ Fundamentals/Classes/Carro.cs
@@ -0,0 +1,23 @@
+﻿using Fundamentals.Enums;
+
+namespace Fundamentals.Classes;
+public class Carro
+{
+    public string Chassi { get; set; }
+
+    public DateOnly Comprado { get; set; }
+
+    public Cor Cor { get; set; }
+
+    public void NumeroDoChassi() => Console.WriteLine(Chassi);
+    public void CompradoEm() => Console.WriteLine(Comprado);
+    public void CorDoCarro() => Console.WriteLine(Cor);
+
+
+    public Carro(String chassi, DateOnly comprado, Cor cor)
+    {
+        Chassi = chassi;
+        Comprado = comprado;
+        Cor = cor;
+    }
+}
