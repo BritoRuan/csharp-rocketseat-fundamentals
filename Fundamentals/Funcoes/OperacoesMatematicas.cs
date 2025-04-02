4532eb419f41a8712308cb865b8e7ecd65262ad4
Ruan Brito <ruan.desenvolvedor@gmail.com>
2025-04-02 17:58:43 -0300

    feat: added optional method

--- Fundamentals/Funcoes/OperacoesMatematicas.cs
+++ Fundamentals/Funcoes/OperacoesMatematicas.cs
@@ -9,4 +9,9 @@ public class OperacoesMatematicas
 
         return (resultado, "Ruan");
     }
+
+    public void Opcional(int valor1, int valor2 = 20)
+    {
+        Console.WriteLine(valor1 + valor2);
+    }
 }
