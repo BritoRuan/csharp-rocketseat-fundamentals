3005d2c4262aa7e9e8199b81f3191a02285e328c
Ruan Brito <ruan.desenvolvedor@gmail.com>
2025-04-03 23:51:00 -0300

    feat: added ternaries

---
+++ Fundamentals/Condicionais/Ternario.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Fundamentals.Condicionais;
+
+class Ternario
+{
+    int numero = 20;
+
+    // string autor = numero == 20 ? "Ruan" : "Matheus";
+    // Console.WriteLine(autor); // Ruan
+}
