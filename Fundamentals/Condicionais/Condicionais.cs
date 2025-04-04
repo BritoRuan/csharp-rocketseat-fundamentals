60ab1cc5fc3eb68289c64992d636f99cef913be7
Ruan Brito <ruan.desenvolvedor@gmail.com>
2025-04-03 23:50:42 -0300

    feat: added conditionals

---
+++ Fundamentals/Condicionais/Condicionais.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Fundamentals.Condicionais;
+
+class Condicionais
+{
+    /*
+     * > Maior
+     * < Menor
+     * >= Maior ou igual
+     * <= Menor ou igual
+     * != // is not Diferente
+     * == // is Igual
+     * && E
+     * || OU
+    */
+
+
+    int numero = 20;
+
+    /* 
+    
+    if (numero > 0) {
+      Console.WriteLine("O número é positivo");
+    } else if (numero == 0){
+      Console.WriteLine("O número é neutro");
+    } else {
+      Console.WriteLine("O número é negativo");
+    }
+   
+    */
+
+    List<string> nomes = new List<string>() { "Ruan", "Mateus", "Samuel" };
+
+    /*
+     
+    if (nomes.Contains("Lucas"))
+    {
+      Console.WriteLine("Lucas está na lista");
+    }
+    else
+    {
+        Console.WriteLine("Lucas não está na lista");
+    }
+
+    */
+}
