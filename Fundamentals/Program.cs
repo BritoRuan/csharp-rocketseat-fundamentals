8112a70688d51710701f8b1a16f46b3cf8d97168
Ruan Brito <ruan.desenvolvedor@gmail.com>
2025-04-04 00:04:39 -0300

    feat: added switch case

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -1,4 +1,5 @@
-﻿using Fundamentals.Classes;
+﻿
+using Fundamentals.Switch;
 
 namespace Fundamentals;
 
@@ -6,18 +7,6 @@ class Program
 {
     static void Main()
     {
-        //carro.Cor = Enums.Cor.Azul;
-        //carro.Comprado = new DateOnly(2024, 12, 25);
-
-        //var carro = new Carro
-        //{
-        //    Cor = Enums.Cor.Azul,
-        //    Comprado = new DateOnly(2024, 12, 25),
-        //    Chassi = "451 X7ngSF L2 CT7763",
-        //};
-
-        Carro meuCarro = new("451 X7ngSF L2 CT7763", new DateOnly(2024, 12, 25) , Enums.Cor.Azul);
-
-        meuCarro.CompradoEm();
+       SwitchCase.Execute();
     }
 }
---
+++ Fundamentals/Switch/Switch.cs
@@ -0,0 +1,47 @@
+﻿
+using Fundamentals.Enums;
+
+namespace Fundamentals.Switch;
+
+class SwitchCase
+{
+    public static void Execute()
+    {
+        string name = "Ruan";
+        int idade = 20;
+
+        // Switch com variável 
+        
+        string resultado = idade switch
+        {
+            10 => "Idade é 10",
+            20 => "Idade é 20",
+            _ => "Idade não é 10 nem 20"
+        };
+
+        Console.WriteLine(resultado);
+
+
+        Cor corFavorita = Cor.Azul;
+        
+        switch(corFavorita)
+        {
+            case Cor.Azul:
+                {
+                    Console.WriteLine("A cor favorita é azul");
+                }
+                break;
+
+            case Cor.Vermelho:
+                {
+                    Console.WriteLine("A cor favorita é vermelho");
+                }
+                break;
+            default:
+                {
+                    Console.WriteLine("A cor favorita é outra");
+                }
+                break;
+        }
+    }
+}
