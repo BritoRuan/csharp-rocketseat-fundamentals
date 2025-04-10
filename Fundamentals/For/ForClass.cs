2a5da30d7dc6a8043b37204895ac9826946d4d89
Ruan Brito <ruan.desenvolvedor@gmail.com>
2025-04-10 12:50:31 -0300

    feat: added for fundamentals

---
+++ Fundamentals/For/ForClass.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Fundamentals.For;
+
+class ForClass
+{
+   public static void Execute ()
+    {
+        for (int i = 10; i > 0; i--)
+        {
+            Console.WriteLine($"Número: {i}");
+        }
+
+        List<string> amigos = new List<string> { "Antonio", "Érico", "Samuel" };
+        
+        
+        for (int i = 0; i < amigos.Count; i++)
+        {
+            Console.WriteLine($"Meu amigo da vez é: {amigos[i]}");
+        }
+    }
+}
---
+++ Fundamentals/For/ForEachClass.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Fundamentals.For;
+
+class ForEachClass
+{
+    public static void Execute()
+    {
+        var amigos = new List<string> { "Antonio", "Érico", "Samuel" };
+
+        Console.WriteLine("Usando o foreach");
+        foreach (var amigo in amigos)
+        {
+            Console.WriteLine($"Meu amigo da vez é: {amigo}");
+        }
+
+
+        Console.WriteLine("Usando o for i");
+
+        for (int i = 0; i < amigos.Count; i++)
+        {
+            Console.WriteLine($"Meu amigo da vez é: {amigos[i]}");
+        }
+
+        var dicionario = new Dictionary<string, string>();
+
+        dicionario.Add("Nome1", "Antonio");
+        dicionario.Add("Nome2", "Samuel");
+        dicionario.Add("Nome3", "Érico");
+
+        
+        Console.WriteLine("Usando o foreach no dicionário");
+        foreach (var item in dicionario)
+        {
+            Console.WriteLine($"Meu amigo da vez é: {item.Key}");
+            Console.WriteLine($"Meu amigo da vez é: {item.Value}");
+        }
+    }
+}
