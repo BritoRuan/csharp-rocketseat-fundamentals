2216cd21c1b1a1aeac743a1a7df1c38514fc101f
Ruan Brito <ruan.desenvolvedor@gmail.com>
2025-04-10 12:51:05 -0300

    feat: added while fundamentals

---
+++ Fundamentals/While/WhileClass.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Fundamentals.While;
+
+class WhileClass
+{
+    public static void Execute()
+    {
+        var idade = 3;
+
+        // Começa com uma checagem
+        while (idade < 3)
+        {
+            Console.WriteLine("Você é menor de idade!");
+
+            idade++;
+        }
+
+        // Executa pelo menos uma vez e depois checar no final
+        do
+        {
+            Console.WriteLine("Você é maior de idade!");
+            idade++;
+        } while (idade < 3);
+    }
+}
